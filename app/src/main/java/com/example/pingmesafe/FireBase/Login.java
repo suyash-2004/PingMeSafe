5e87f3f7f512ffa5fb45244f18b91b04ab54df8e
naitiklovestech <singhalnaitik22@gmail.com>
2024-04-28 10:55:58 +0530

    Added Registration page

--- app/src/main/java/com/example/pingmesafe/FireBase/Login.java
+++ app/src/main/java/com/example/pingmesafe/FireBase/Login.java
@@ -16,7 +16,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
